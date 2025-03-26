24db6694f7a67da8a1a8df837aeb879c5c9bccec
unknown <brandonalcantara17>
2025-03-26 16:37:46 +0100

    3

--- main.py
+++ main.py
@@ -1,11 +1,70 @@
 import requests
 import json
+import time
 
 url = "http://localhost:8080"
+eliminated = ""
 
-game = input("Game number: ")
-result_game = requests.get(f"{url}/partida/{game}")
-print(json.dumps(result_game.json(), indent=2))
+def participants():
+    global eliminated
+    result_jugadors = requests.get(f"{url}/jugadors")
+    players = result_jugadors.json()
+    
+    print("\033[32mParticipants:\033[0m")
+    time.sleep(0.6)
+    for player in players:
+        time.sleep(0.1)
+        words = player.split()
+        name = ""
+        country = ""
+        
+        for i in range(len(words)):
+            if words[i].lower() == "participant":
+                name = " ".join(words[i+1:i+3]).replace(",", "").replace(".", "")
+            elif words[i].lower() == "representa":
+                country = words[i+2].replace(",", "").replace(".", "")
+            elif words[i].lower() == "representant":
+                country = words[i+2].replace(",", "").replace(".", "")
+        
+        if "desqualificada" in player.lower():
+            eliminated = name
 
-#result_jugadors = requests.get(f"{url}/jugadors")
-#print(result_jugadors.json())
+        print(f"{name} de {country}")
+
+    print(f"eliminated: {eliminated}")
+
+def guanyador():
+    global eliminated
+    print("\033[32mGuanyador:\033[0m")
+    for counter in range(1, 10001):
+        result_game = requests.get(f"{url}/partida/{counter}")
+        game = result_game.json()
+        table = game["tauler"]
+
+        def winner(player):
+            print(f"Guanya el {game[player]}")
+
+        for row in table:
+            if row == "OOO":
+                winner('jugador1')
+            elif row == "XXX":
+                winner('jugador2')
+
+        for col in range(3):
+            if table[0][col] + table[1][col] + table[2][col] == "OOO":
+                winner('jugador1')
+            elif table[0][col] + table[1][col] + table[2][col] == "XXX":
+                winner('jugador2')
+            
+        diag1 = table[0][0] + table[1][1] + table[2][2]
+        diag2 = table[0][2] + table[1][1] + table[2][0]
+        
+        if diag1 == "OOO" or diag2 == "OOO":
+            winner('jugador1')
+        elif diag1 == "XXX" or diag2 == "XXX":
+            winner('jugador2')
+        else:
+            print("No hi ha guanyador")
+
+participants()
+guanyador()
